87d821c350978061ea7333abbd857bd1140bfb28
elena <elesanalb@gmail.com>
2024-11-01 09:39:16 +0100

    logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import logo from './assets/cart-icon.svg'
 
 function Header({title}) {
 
     return (
         <>
             <header>
+                <img src={logo} alt="cart icon" className='logo'/>
                 <h1 className='title'>{title}</h1>
             </header>
         </>
